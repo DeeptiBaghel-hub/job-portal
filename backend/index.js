cd5304a1edf8512199dd94a3b5f2c89174dcd9d7
DeeptiBaghel-hub <sagar.deepti.baghel@gmail.com>
2026-09-18 00:09:49 +0530

    feat: add backend application workflows

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,54 @@
-//const express=require(express) //old
-import dns from "node:dns";
-
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
-import comapnyRoute from "./routes/company.route.js";
+import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
-
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
-const app=express();
-//middleware
+const app = express();
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions={
+const corsOptions = {
     origin:'http://localhost:5173',
-    credentials:true,
-
+    credentials:true
 }
+
 app.use(cors(corsOptions));
 
-const PORT=process.env.PORT || 3000;
-
-//apis
-app.use("/api/v1/user",userRoute);
-/*"http://localhost8000/api/v1/user/register"
-"http://localhost8000/api/v1/user/login"
-"http://localhost8000/api/v1/user/profile/update"*/
+const PORT = process.env.PORT || 3000;
 
 
-app.use("/api/v1/comapany",comapnyRoute);
-app.use("/api/v1/comapany",jobRoute);
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+
 
 app.listen(PORT,()=>{
     connectDB();
-    console.log(`server running at port ${PORT}`)
+    console.log(`Server running at port ${PORT}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+/*"http://localhost8000/api/v1/user/register"
+"http://localhost8000/api/v1/user/login"
+"http://localhost8000/api/v1/user/profile/update"*/
